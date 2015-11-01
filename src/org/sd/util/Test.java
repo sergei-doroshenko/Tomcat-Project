89e5c9c0d847e452ca0379e8bcd73d4509e8199e
sergei-doroshenko <6823298@gmail.com>
2015-11-01 20:43:00 +0200

    First commit.

---
+++ src/org/sd/util/Test.java
@@ -0,0 +1,13 @@
+package org.sd.util;
+
+import java.io.File;
+
+/**
+ * Created by Sergei 1 on 01.11.2015.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(new File(System.getProperty("user.dir") + "\\out\\servlet\\PrimitiveServlet.class"));
+
+    }
+}
---
+++ src/ver_02/StaticResourceProcessor.java
@@ -0,0 +1,15 @@
+package ver_02;
+
+import java.io.IOException;
+
+public class StaticResourceProcessor {
+
+  public void process(Request request, Response response) {
+    try {
+      response.sendStaticResource();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
